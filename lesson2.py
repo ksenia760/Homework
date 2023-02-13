07b89ebc83fa68e0238519aee1d4d057c4dc52c0
ksenia760 <ksu.perederenko@gmail.com>
2023-02-13 12:04:38 +0200

    homework

---
+++ lesson2.py
@@ -0,0 +1,15 @@
+
+n = int(input("Your num of apples: "))
+k = int(input("Your num of children: "))
+print("Apples for children", n % k)
+print("the rest of apples", n // k)
+
+
+math_class1 = int(input("Num of pupils1: "))
+math_class2 = int(input("Num of pupils2: "))
+math_class3 = int(input("Num of pupils3: "))
+print(math_class1 // 2 + math_class2 // 2 + math_class3 // 2 + math_class1 % 2 + math_class2 % 2 + math_class3 % 2)
+
+m = "123456"
+new_number = list(reversed(m))
+print("Result:", new_number)
