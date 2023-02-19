c26b584d8ad8cd5eca7b111e52e7268ae3403f64
ksenia760 <ksu.perederenko@gmail.com>
2023-02-19 19:39:11 +0200

    homework5

---
+++ lesson_03.py
@@ -0,0 +1,50 @@
+# task N6
+n = 50
+a = 1
+while a**2 <= n:
+    print("num 50: ", a**2)
+    a = a + 1
+    continue
+
+n = 10
+a = 1
+while a**2 <= n:
+    print("num 10: ", a**2)
+    a = a + 1
+    continue
+
+n = 9
+a = 1
+while a**2 <= n:
+    print("num 9: ",  a**2)
+    a = a + 1
+    continue
+
+n = 4
+a = 1
+while a**2 <= n:
+    print("num 4: ", a**2)
+    a = a + 1
+    continue
+
+n = 1
+a = 1
+while a**2 <= n:
+    print("num 1: ", a**2)
+    a = a + 1
+    continue
+
+n = 100
+a = 1
+while a**2 <= n:
+    print("num 100: ", a**2)
+    a = a + 1
+    continue
+
+n = 99
+a = 1
+while a**2 <= n:
+    print("num 99: ", a**2)
+    a = a + 1
+
+
