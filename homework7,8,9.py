8c2858dd30992230e3fda5e0570c1bd03647df8e
ksenia760 <ksu.perederenko@gmail.com>
2023-02-21 23:43:38 +0200

    homework7,8,9.py

---
+++ homework7,8,9.py
@@ -0,0 +1,23 @@
+# task N7
+n = "Beautiful weather outside "
+words = n.split(" ")
+num_words = len(words)
+print(num_words)
+
+# task N8
+s = input("Enter a line: ")
+ch = input("Enter a character: ")
+print(s.find(ch))
+
+# task N9
+string = "Hello world!"
+print(string[2])
+print(string[10])
+print(string[:5])
+print(string[:-2])
+print("even indices", string[::2])
+print("not even indices", string[1::2])
+print(string[::-1])
+print(string[::-2])
+print(len(string))
+
